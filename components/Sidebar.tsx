991d0ba67832e59cf8d3a784c6e0f43630fc0b96
iknow6aint <saintagbukor@gmail.com>
2023-09-26 22:02:50 +0100

    sidebr-item

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,10 @@
+'use client'
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import SidebarItem from "./SidebarItem";
+
 import Box from "./Box";
 interface SidebarProps {
     children: React.ReactNode;
@@ -41,7 +44,14 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
             "
             >
                 <Box>
-                    side bsr navigation
+                    <div className="flex flex-col gap-y-4 px-5 py-4">
+                        {routes.map((item) => (
+                            <SidebarItem key={item.label} {...item} />
+                        ))}
+                    </div>
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    Lib
                 </Box>
             </div>
         </div>
