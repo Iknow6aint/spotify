d3ca95a95b275529188203fc220c243b408ecc7f
iknow6aint <saintagbukor@gmail.com>
2023-09-27 12:23:14 +0100

    sidebar item

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,12 @@
+'use client'
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import SidebarItem from "./SidebarItem";
+
+import Box from "./Box";
+import Library from "./Library";
 interface SidebarProps {
     children: React.ReactNode;
 }
@@ -39,7 +44,16 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
                 p-2
             "
             >
-
+                <Box>
+                    <div className="flex flex-col gap-y-4 px-5 py-4">
+                        {routes.map((item) => (
+                            <SidebarItem key={item.label} {...item} />
+                        ))}
+                    </div>
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    <Library />
+                </Box>
             </div>
         </div>
     )
