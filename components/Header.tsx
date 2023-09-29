feb3049d1343c8134af616b1cfdeb2c40b57a09c
iknow6aint <saintagbukor@gmail.com>
2023-09-29 20:19:33 +0100

    supabase provider

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { useRouter } from "next/navigation";
 import { FaUserAlt } from "react-icons/fa";
 // import { useSupabaseClient } from '@supabase/auth-helpers-react';
-import { toast } from "react-hot-toast";
+// import { toast } from "react-hot-toast";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 
