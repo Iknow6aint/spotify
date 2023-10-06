779bfb88c72c48bc2ed6f7f083c7ff18f22b8134
iknow6aint <saintagbukor@gmail.com>
2023-10-06 15:29:31 +0100

    upload modal, input ,useUpload modal

--- components/Library.tsx
+++ components/Library.tsx
@@ -2,8 +2,24 @@
 
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
+import { useUser } from "@/hooks/useUser";
+import useAuthModal from "@/hooks/useAuthModal";
 
 const Library = () => {
+
+    const { user, subscription } = useUser();
+    const authModal = useAuthModal();
+    const onClick = () => {
+        if (!user) {
+            return authModal.onOpen();
+        }
+
+        // if (!subscription) {
+        //     return subscribeModal.onOpen();
+        // }
+
+        // return uploadModal.onOpen();
+    }
     return (
         <div className="flex flex-col">
             <div className="flex items-center justify-between px-5 pt-4">
@@ -14,7 +30,7 @@ const Library = () => {
                     </p>
                 </div>
                 <AiOutlinePlus
-                    // onClick={onClick}
+                    onClick={onClick}
                     size={20}
                     className="
             text-neutral-400 
